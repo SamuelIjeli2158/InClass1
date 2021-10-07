752869bb2e7d92b89ff561ccafa5b6b8af8f858a
samuel ijeli <ijeli@sheridancollege.ca>
2021-10-06 20:39:20 -0400

    second commit and created local branch

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,14 +1,26 @@
-
+// student ID: 991602158
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Samuel Ijeli
  */
 public class CardTrick {
+    private int cardNumber; 
     public static void main(String[] args)
     {
-       
+     
+     
+          
+          
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
     }
     
 }
